a86b6aca36b8cdf4b3c2b007757552f2ad4a3ca9
SkylarKim51 <32948931+SkylarKim51@users.noreply.github.com>
2019-11-22 15:41:26 -0800

    part1 complete

---
+++ GccApplication1/GccApplication1/main.c
@@ -0,0 +1,103 @@
+/*
+ * GccApplication1.c
+ *
+ * Created: 11/21/2019 3:04:08 PM
+ * Author : skylar kim
+ */ 
+
+#include <avr/io.h>
+#include "io.h"
+#include "keypad.h"
+#include "scheduler.h"
+#include "timer.h"
+#include <avr/interrupt.h>
+#include "bit.h"
+
+unsigned char tmpB = 0x00;
+
+enum SM1_States{SM1_output};
+
+int SMTick1(int state){
+	unsigned char x;
+	x = GetKeypadKey();
+	switch(state){
+		case SM1_output:
+		switch (x) {
+			case '\0': tmpB = 0x1F; break; // All 5 LEDs on
+			case '1': tmpB = 0x01; break; // hex equivalent
+			case '2': tmpB = 0x02; break;
+			case '3': tmpB = 0x03; break;
+			case '4': tmpB = 0x04; break;
+			case '5': tmpB = 0x05; break;
+			case '6': tmpB = 0x06; break;
+			case '7': tmpB = 0x07; break;
+			case '8': tmpB = 0x08; break;
+			case '9': tmpB = 0x09; break;
+			case 'A': tmpB = 0x0A; break;
+			case 'B': tmpB = 0x0B; break;
+			case 'C': tmpB = 0x0C; break;
+			case 'D': tmpB = 0x0D; break;
+			case '*': tmpB = 0x0E; break;
+			case '0': tmpB = 0x00; break;
+			case '#': tmpB = 0x0F; break;
+			default: tmpB = 0x1B; break; // Should never occur. Middle LED off.
+		}
+		state = SM1_output;
+		PORTB=tmpB;
+		break;
+	}
+	return state;
+}
+
+
+int main(void)
+{
+	DDRB = 0xFF;	PORTB = 0x00;
+	DDRC = 0xF0;	PORTC = 0x0F;
+	unsigned long int SMTick1_calc = 20;
+
+
+	//Calculating GCD
+	unsigned long int tmpGCD = 10;
+
+	//Greatest common divisor for all tasks or smallest time unit for tasks.
+	unsigned long int GCD = tmpGCD;
+
+	//Recalculate GCD periods for scheduler
+	unsigned long int SMTick1_period = SMTick1_calc/GCD;
+
+	//Declare an array of tasks
+	static task task1;
+	task *tasks[] = { &task1};
+	const unsigned short numTasks = sizeof(tasks)/sizeof(task*);
+
+	// Task 1
+	task1.state = 0;//Task initial state.
+	task1.period = SMTick1_period;//Task Period.
+	task1.elapsedTime = SMTick1_period;//Task current elapsed time.
+	task1.TickFct = &SMTick1;//Function pointer for the tick.
+
+
+	// Set the timer and turn it on
+	TimerSet(GCD);
+	TimerOn();
+	
+	unsigned short i;
+    while (1) 
+    {
+		// Scheduler code
+		for ( i = 0; i < numTasks; i++ ) {
+			// Task is ready to tick
+			if ( tasks[i]->elapsedTime == tasks[i]->period ) {
+				// Setting next state for task
+				tasks[i]->state = tasks[i]->TickFct(tasks[i]->state);
+				// Reset the elapsed time for next tick.
+				tasks[i]->elapsedTime = 0;
+			}
+			tasks[i]->elapsedTime += 1;
+		}
+		while(!TimerFlag);
+		TimerFlag = 0;				
+    }
+}
+
